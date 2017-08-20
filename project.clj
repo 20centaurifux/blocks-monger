3a643b33aca7c6b963991d3a13c8afa5be74cfa7
Sebastian Fedrau <sebastian.fedrau@gmail.com>
2017-08-20 13:09:08 +0200

    turn off JDBC driver logging by default

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
             :url "https://www.gnu.org/licenses/gpl-3.0.txt"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [mvxcvi/blocks "0.9.1"]
-                 [com.novemberain/monger "3.1.0"]]
+                 [com.novemberain/monger "3.1.0"]
+                 [org.slf4j/slf4j-nop "1.7.12"]]
   :profiles {:test {:dependencies [[org.clojure/test.check "0.9.0"]
                                    [mvxcvi/puget "1.0.1"]]}})
 
