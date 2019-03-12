4d2f3cb670ea1b33618dba0ac63f2ce0e425aaee
Sebastian Fedrau <sebastian.fedrau@gmail.com>
2019-03-12 19:25:52 +0100

    upgrade to blocks 2.x

--- src/blocks/store/monger.clj
+++ src/blocks/store/monger.clj
@@ -1,57 +1,51 @@
 (ns blocks.store.monger
   "MongoDB storage backend for blocks."
-  (:require (blocks
-              [core :as block]
-              [data :as data]
-              [store :as store])
-            (monger
-              [core :as mg]
-              [collection :as mc]
-              [query :as query]
-              [operators :refer :all]
-              [credentials :as mcred])
-            [multihash.core :as mhash]
-            [severin.core :as severin])
-  (:use [severin.pool.monger]))
+  (:require
+   [blocks.core :as block]
+   [blocks.data :as data]
+   [blocks.store :as store]
+   [alphabase.hex :as hex]
+   [manifold.stream :as s]
+   [multiformats.hash :as multihash]
+   [monger.collection :as mc]
+   [monger.query :as query]
+   [monger.operators :refer :all]
+   [severin.core :as severin])
+  (:use [severin.pool.monger])
+  (:import blocks.data.Block))
 
 (def pool (severin/make-pool))
 
 (defn- block->base64
   "Reads all bytes from block and returns a base64 encoded string."
-  [block]
+  [^Block block]
   (let [dst (java.io.ByteArrayOutputStream.)]
-    (clojure.java.io/copy (block/open block) dst)
+    (block/write! block dst)
     (.encodeToString (java.util.Base64/getEncoder) (.toByteArray dst))))
 
 (defn- base64->bytes
-  "Decodes the string b64 to bytes."
+  "Decodes a base64 encoded string to bytes."
   [^String b64]
   (.decode (^java.util.Base64Decoder java.util.Base64/getDecoder) (.getBytes b64)))
 
+(defn- base64->stream
+  "Creates an input stream providing a decoded base64 string."
+  [^String b64]
+  (java.io.ByteArrayInputStream. (base64->bytes b64)))
+
 (defn- doc->block
-  "Converts doc to a block."
-  [doc]
-  (when doc
-    (let [id (mhash/decode (:id doc))]
-      (-> (data/literal-block id (base64->bytes (:blob doc)))
-          (block/with-stats (merge {:id id} (:stats doc)))))))
-
-(defn- opts->query
-  "Converts blocks search options to a monger query."
-  [opts]
-  (merge
-    (query/empty-query)
-    (hash-map :sort {:id 1} :fields [:id :stats])
-    (select-keys opts [:limit])
-    (->> (cond-> (select-keys opts [:algorithm])
-           (:after opts) (assoc :id {$gt (:after opts)}))
-         (hash-map :query))))
-
-(defn- execute-query
-  "Executes the query m and returns a cursor."
-  [^com.mongodb.DB db collname m]
-  (let [coll (.getCollection db collname)]
-    (query/exec (merge {:collection coll} m))))
+  "Converts a mongodb document to a block."
+  ([doc]
+   (-> (:id doc)
+       (hex/decode)
+       (multihash/decode)
+       (doc->block doc)))
+  ([id doc]
+   (when-let [stats (:stats doc)]
+     (data/create-block id
+                        (:size stats)
+                        (.toInstant (:stored-at stats))
+                        #(base64->stream (:blob doc))))))
 
 (defn- connect
   "Connects to the MongoDB server. Returns the connection and database instance."
@@ -70,75 +64,111 @@
      (severin/dispose! pool r#)
      result#))
 
-(defn- doc->stats
-  "Returns the stats from a document and adds the id as multihash to the map."
-  [doc]
-  (when doc
-    (assoc (:stats doc) :id (mhash/decode (:id doc)))))
+(defn- execute-query
+  "Executes the query m and returns a cursor."
+  [^com.mongodb.DB db collname m]
+  (let [coll (.getCollection db collname)]
+    (query/exec (merge {:collection coll} m))))
 
 (defn- cur->seq
   "Converts a MongoDB cursor to a lazy sequence. The connection is closed when the
   sequence is realized."
   [cur r]
   (lazy-seq
-    (if-let [doc (first cur)]
-      (cons (doc->stats doc)
-            (cur->seq (rest cur) r))
-      (severin/dispose! pool r))))
+   (if-let [doc (first cur)]
+     (cons (doc->block doc)
+           (cur->seq (rest cur) r))
+     (severin/dispose! pool r))))
 
-(defrecord MongerBlockStore
-  []
+(defn- doc->stats
+  "Returns the stats from a document."
+  [id doc]
+  (when-let [stats (:stats doc)]
+    {:id id
+     :size (:size stats)
+     :stored-at (.toInstant (:stored-at stats))}))
+
+(defn- opts->query
+  "Converts blocks search options to a monger query."
+  [opts]
+  (merge
+   (query/empty-query)
+   (hash-map :sort {:id 1}
+             :fields [:id :stats :blob :algorithm])
+   (select-keys opts [:limit])
+   (->> (cond-> (select-keys opts [:algorithm])
+          (:after opts) (assoc :id {$gt (:after opts)}))
+        (hash-map :query))))
+
+(defn- prepare-block
+  "Prepares a new block for storage based on the given block. This ensures the
+  content is loaded into memory and cleans the block metadata."
+  [^Block block]
+  (if (data/byte-content? block)
+    (data/create-block
+     (:id block)
+     (:size block)
+     (.content block))
+    (data/read-block
+     (:algorithm (:id block))
+     (data/read-all (.content block)))))
+
+(defrecord MongerBlockStore []
 
   store/BlockStore
-
-  (-stat
-    [this id]
-    (-> (with-db this (mc/find-one-as-map "blocks" {:id (mhash/hex id)} [:id :stats]))
-        (doc->stats)))
 
   (-list
     [this opts]
     (let [r (connect this)]
       (-> (execute-query (:db r) "blocks" (opts->query opts))
-          (cur->seq r))))
+          (cur->seq r)
+          (s/->source))))
+
+  (-stat
+    [this id]
+    (store/future'
+     (->> (with-db this (mc/find-one-as-map "blocks" {:id (multihash/hex id)} [:stats]))
+          (doc->stats id))))
 
   (-get
     [this id]
-    (-> (with-db this (mc/find-one-as-map "blocks" {:id (mhash/hex id)}))
-        (doc->block)))
+    (store/future'
+     (->> (with-db this (mc/find-one-as-map "blocks" {:id (multihash/hex id)}))
+          (doc->block id))))
 
   (-put!
     [this block]
-    (let [id (:id block)
-          hex (mhash/hex id)]
-      (with-db
-        this
-        (#(if-let [doc (mc/find-one-as-map % "blocks" {:id hex})]
-           (doc->block doc)
-           (let [stats {:stored-at (java.util.Date.)
-                        :size (:size block)}]
-             (mc/insert %
-                        "blocks"
-                        {:id hex
-                         :blob (block->base64 block)
-                         :stats stats
-                         :algorithm (:algorithm id)})
-             (block/with-stats block stats)))))))
+    (store/future'
+     (let [id (:id block)
+           hex (multihash/hex id)]
+       (with-db
+         this
+         (#(if-let [doc (mc/find-one-as-map % "blocks" {:id hex})]
+             (doc->block id doc)
+             (let [block' (prepare-block block)]
+               (mc/insert %
+                          "blocks"
+                          {:id hex
+                           :blob (block->base64 block')
+                           :stats {:stored-at (java.util.Date/from (:stored-at block'))
+                                   :size (:size block')}
+                           :algorithm (:algorithm id)})
+               block')))))))
 
   (-delete!
     [this id]
-    (when-let [hex (mhash/hex id)]
-      (-> (with-db this (mc/remove "blocks" {:id hex}))
-          (.getN)
-          (not= 0))))
+    (store/future'
+     (when-let [hex (multihash/hex id)]
+       (-> (with-db this (mc/remove "blocks" {:id hex}))
+           (.getN)
+           (not= 0)))))
 
   store/ErasableStore
 
   (-erase!
     [this]
-    (with-db this (mc/remove "blocks" {}))))
-
-(store/privatize-constructors! MongerBlockStore)
+    (store/future'
+     (with-db this (mc/remove "blocks" {})))))
 
 (defn monger-block-store
   "Creates a new Monger block store.
--- test/blocks/store/monger_test.clj
+++ test/blocks/store/monger_test.clj
@@ -1,8 +1,16 @@
 (ns blocks.store.monger-test
   (:require
-    [blocks.store.tests :as tests]
-    [blocks.store.monger :refer [monger-block-store]]
-    [clojure.test :refer :all]))
+   [blocks.core :as block]
+   [blocks.store.tests :as tests]
+   [blocks.store.monger :refer [monger-block-store]]
+   [monger.core :as monger]
+   [clojure.test :refer :all])
+  (:import com.mongodb.WriteConcern))
+
+(monger/set-default-write-concern! WriteConcern/ACKNOWLEDGED)
 
 (deftest ^:integration test-monger-store
-   (tests/check-store! #(monger-block-store :uri "monger://localhost/monger-store-test")))
+  (tests/check-store #(let [store (monger-block-store
+                                   :uri "monger://localhost/monger-store-test?fsync=true")]
+                        @(block/erase! store)
+                        store)))
